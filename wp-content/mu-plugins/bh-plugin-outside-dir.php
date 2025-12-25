9e22e7f901a76e2090d30d49c101efc2ce2232c4
Brian Henry <BrianHenryIE@gmail.com>
2025-12-24 21:21:36 -0800

    `phpcbf`

--- wp-content/mu-plugins/bh-plugin-outside-dir.php
+++ wp-content/mu-plugins/bh-plugin-outside-dir.php
@@ -6,20 +6,26 @@
  *
  * I'm currently setting `$arbitrary_plugins` in `/tests/bootstrap.php`.
  */
-
-function activate_plugin_at_arbitrary_path( string $plugin_file_path, ?string $plugin_basename = null ) {
-	if(!file_exists($plugin_file_path)) {
+function activate_plugin_at_arbitrary_path( string $plugin_file_path, ?string $plugin_basename = null ): void {
+	if ( ! file_exists( $plugin_file_path ) ) {
 		return;
 	}
 
 	wp_register_plugin_realpath( $plugin_file_path );
 
-	$plugin_basename = $plugin_basename ?? basename(dirname($plugin_file_path)) . '/' . basename( $plugin_file_path );
+	$plugin_basename = $plugin_basename ?? basename( dirname( $plugin_file_path ) ) . '/' . basename( $plugin_file_path );
 
-	add_filter("option_active_plugins", function($plugins) use ($plugin_basename ){
-		$plugins[] = 'bh-wp-bitcoin-gateway/bh-wp-bitcoin-gateway.php';
-		return $plugins;
-	});
+	/**
+	 * @see get_option()
+	 */
+	add_filter(
+		'option_active_plugins',
+		function ( $plugins ) use ( $plugin_basename ): array {
+			$plugins   = array_filter( (array) $plugins );
+			$plugins[] = $plugin_basename;
+			return $plugins;
+		}
+	);
 
 	// Actually load the plugin.
 	include_once $plugin_file_path;
@@ -27,8 +33,6 @@ function activate_plugin_at_arbitrary_path( string $plugin_file_path, ?string $p
 
 global $arbitrary_plugins;
 
-foreach((array) $arbitrary_plugins as $plugin_file_path) {
-	activate_plugin_at_arbitrary_path($plugin_file_path);
+foreach ( (array) $arbitrary_plugins as $plugin_file_path ) {
+	activate_plugin_at_arbitrary_path( $plugin_file_path );
 }
-
-
