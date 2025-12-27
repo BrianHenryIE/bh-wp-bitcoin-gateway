6c6643792e972c1880ab8f06dfc4b0cf9cbaa95a
Brian Henry <BrianHenryIE@gmail.com>
2025-12-26 23:30:34 -0800

    PHPStan/phpcs

--- wp-content/mu-plugins/bh-plugin-outside-dir.php
+++ wp-content/mu-plugins/bh-plugin-outside-dir.php
@@ -5,13 +5,15 @@
  * MU plugins are loaded after `global $wp_plugin_paths` is set but before normal plugins are loaded.
  *
  * I'm currently setting `$arbitrary_plugins` in `/tests/bootstrap.php`.
+ *
+ * @package brianhenryie/bh-wp-bitcoin-gateway
  */
 
 /**
  * `include`s the plugin file, and adds a filter so `option_active_plugins` shows it is active.
  *
- * @param string      $plugin_file_path
- * @param string|null $plugin_basename
+ * @param string      $plugin_file_path Absolute path to main plugin file.
+ * @param string|null $plugin_basename `dir/file.php` for plugin from WP_PLUGIN_DIR.
  */
 function activate_plugin_at_arbitrary_path( string $plugin_file_path, ?string $plugin_basename = null ): void {
 	if ( ! file_exists( $plugin_file_path ) ) {
@@ -38,6 +40,7 @@ function activate_plugin_at_arbitrary_path( string $plugin_file_path, ?string $p
 	include_once $plugin_file_path;
 }
 
+/** @var null|array<string> $arbitrary_plugins Array of absolute paths to plugins' main files, if defined. */
 global $arbitrary_plugins;
 
 foreach ( (array) $arbitrary_plugins as $plugin_file_path ) {
