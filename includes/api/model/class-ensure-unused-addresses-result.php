152da87cf3d2e64be7fd0248c30c65d24a772f6d
Brian Henry <BrianHenryIE@gmail.com>
2026-01-02 23:12:38 -0800

    Update class-ensure-unused-addresses-result.php

--- includes/api/model/class-ensure-unused-addresses-result.php
+++ includes/api/model/class-ensure-unused-addresses-result.php
@@ -17,12 +17,14 @@ class Ensure_Unused_Addresses_Result {
 	 * Constructor
 	 *
 	 * @param Bitcoin_Wallet    $wallet The wallet that the new addresses were generated for.
-	 * @param Bitcoin_Address[] $existing_unused_addresses The unused addresses that were already known.
+	 * @param Bitcoin_Address[] $assumed_existing_unused_addresses Wallet's addresses that were saved as unused.
+	 * @param Bitcoin_Address[] $actual_existing_unused_addresses The assumed addresses that were correctly marked as unused.
 	 * @param Bitcoin_Address[] $new_unused_addresses The newly generated unused addresses.
 	 */
 	public function __construct(
 		public Bitcoin_Wallet $wallet,
-		public array $existing_unused_addresses,
+		public array $assumed_existing_unused_addresses,
+		public array $actual_existing_unused_addresses,
 		public array $new_unused_addresses,
 	) {
 	}
