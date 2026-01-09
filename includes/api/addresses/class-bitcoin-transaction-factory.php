04ac9350899c564eb92c155ed1fc3b24d18f0f42
Brian Henry <BrianHenryIE@gmail.com>
2026-01-09 00:05:12 -0800

    Some type checks (PHPStan)

--- includes/api/addresses/class-bitcoin-transaction-factory.php
+++ includes/api/addresses/class-bitcoin-transaction-factory.php
@@ -7,9 +7,12 @@
 
 namespace BrianHenryIE\WP_Bitcoin_Gateway\API\Addresses;
 
+use BrianHenryIE\WP_Bitcoin_Gateway\API\Model\BH_WP_Bitcoin_Gateway_Exception;
 use BrianHenryIE\WP_Bitcoin_Gateway\API\Model\Transaction;
 use BrianHenryIE\WP_Bitcoin_Gateway\Brick\Money\Money;
 use BrianHenryIE\WP_Bitcoin_Gateway\BtcRpcExplorer\JsonMapper\AssociativeArrayMiddleware;
+use BrianHenryIE\WP_Bitcoin_Gateway\JsonMapper\Exception\BuilderException;
+use BrianHenryIE\WP_Bitcoin_Gateway\JsonMapper\Exception\ClassFactoryException;
 use BrianHenryIE\WP_Bitcoin_Gateway\JsonMapper\Handler\FactoryRegistry;
 use BrianHenryIE\WP_Bitcoin_Gateway\JsonMapper\Handler\PropertyMapper;
 use BrianHenryIE\WP_Bitcoin_Gateway\JsonMapper\JsonMapperBuilder;
@@ -27,7 +30,8 @@ class Bitcoin_Transaction_Factory {
 	 * @param int $post_id The WordPress post id this wallet is stored under.
 	 *
 	 * @throws InvalidArgumentException When the supplied post_id is not a post of this type.
-	 * @throws JsonException
+	 * @throws BuilderException
+	 * @throws ClassFactoryException
 	 */
 	public function get_by_wp_post_id( int $post_id ): Bitcoin_Transaction {
 		$post = get_post( $post_id );
@@ -45,26 +49,53 @@ class Bitcoin_Transaction_Factory {
 	 *
 	 * @param WP_Post $post The backing WP_Post for this Bitcoin_Transaction.
 	 *
-	 * @throws JsonException
+	 * @throws BuilderException
+	 * @throws ClassFactoryException
 	 */
 	public function get_by_wp_post( WP_Post $post ): Bitcoin_Transaction {
 
 		$factory_registry = new FactoryRegistry();
+
 		$factory_registry->addFactory(
 			\DateTimeInterface::class,
 			function ( stdClass $json_object ) {
-				return new \DateTimeImmutable( $json_object->date )
+				if (
+					! property_exists( $json_object, 'date' )
+					|| ! property_exists( $json_object, 'timezone' )
+				) {
+					throw new BH_WP_Bitcoin_Gateway_Exception( 'Invalid json encoded DateTime object.' );
+				}
+				/** @var string $date */
+				$date = $json_object->date;
+				/** @var string $timezone */
+				$timezone = $json_object->timezone;
+				return new \DateTimeImmutable( $date )
 				->setTimezone(
-					new \DateTimeZone( $json_object->timezone )
+					new \DateTimeZone( $timezone )
 				);
 			}
 		);
+
 		$factory_registry->addFactory(
 			Money::class,
 			function ( stdClass $json_object ) {
-				return Money::of( $json_object->amount, $json_object->currency );
+				/**
+				 * @see Money::jsonSerialize()
+				 */
+				if (
+					! property_exists( $json_object, 'amount' )
+					|| ! property_exists( $json_object, 'currency' )
+				) {
+					throw new BH_WP_Bitcoin_Gateway_Exception( 'Invalid json encoded money object.' );
+				}
+				/** @var string $amount */
+				$amount = $json_object->amount;
+				/** @var string $currency */
+				$currency = $json_object->currency;
+				return Money::of( $amount, $currency );
 			}
 		);
+
 		$property_mapper = new PropertyMapper( $factory_registry );
 		$mapper          = JsonMapperBuilder::new()
 			->withPropertyMapper( $property_mapper )
