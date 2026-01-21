4395b51b76e715950ab1b25d784565f0106e3d58
Brian Henry <BrianHenryIE@gmail.com>
2026-01-21 13:55:14 -0800

    Add contract test to compare internal calculations with API results

--- tests/contract/api/blockchain/class-bitcoin-api-contract-Test.php
+++ tests/contract/api/blockchain/class-bitcoin-api-contract-Test.php
@@ -10,67 +10,56 @@
 namespace BrianHenryIE\WP_Bitcoin_Gateway\API\Clients\Blockchain;
 
 use BrianHenryIE\ColorLogger\ColorLogger;
-use BrianHenryIE\WP_Bitcoin_Gateway\Action_Scheduler\Background_Jobs_Actions_Handler;
-use BrianHenryIE\WP_Bitcoin_Gateway\Action_Scheduler\Background_Jobs_Scheduler_Interface;
+use BrianHenryIE\WP_Bitcoin_Gateway\API\Clients\Blockchain_API_Extended_Interface;
+use BrianHenryIE\WP_Bitcoin_Gateway\API\Model\Exceptions\Rate_Limit_Exception;
 use BrianHenryIE\WP_Bitcoin_Gateway\API\Model\Wallet\Bitcoin_Address;
-use BrianHenryIE\WP_Bitcoin_Gateway\API\Repositories\Factories\Bitcoin_Address_Factory;
-use BrianHenryIE\WP_Bitcoin_Gateway\API\Repositories\Bitcoin_Address_Repository;
-use BrianHenryIE\WP_Bitcoin_Gateway\API\Model\Payments\Bitcoin_Transaction;
 use BrianHenryIE\WP_Bitcoin_Gateway\API\Repositories\Factories\Bitcoin_Transaction_Factory;
 use BrianHenryIE\WP_Bitcoin_Gateway\API\Repositories\Bitcoin_Transaction_Repository;
-use BrianHenryIE\WP_Bitcoin_Gateway\API\Repositories\Bitcoin_Wallet_Repository;
-use BrianHenryIE\WP_Bitcoin_Gateway\API\API;
 use BrianHenryIE\WP_Bitcoin_Gateway\API\Clients\Blockchain_API_Interface;
-use BrianHenryIE\WP_Bitcoin_Gateway\API\Clients\Exchange_Rate_API_Interface;
-use BrianHenryIE\WP_Bitcoin_Gateway\API\Helpers\Generate_Address_API_Interface;
 use BrianHenryIE\WP_Bitcoin_Gateway\API\Model\Payments\Transaction_Interface;
-use BrianHenryIE\WP_Bitcoin_Gateway\Settings_Interface;
+use BrianHenryIE\WP_Bitcoin_Gateway\API\Services\Payment_Service;
+use BrianHenryIE\WP_Bitcoin_Gateway\Brick\Money\Exception\MoneyMismatchException;
+use Exception;
+use JsonException;
+use lucatume\WPBrowser\TestCase\WPTestCase;
 use Psr\Log\LoggerInterface;
-use stdClass;
-use WP_Post;
 
 /**
  * @coversNothing
  *
  * Uses WPTestCase because the test uses the Requests library.
  */
-class Bitcoin_API_Contract_Test extends \lucatume\WPBrowser\TestCase\WPTestCase {
+class Bitcoin_API_Contract_Test extends WPTestCase {
 
-	protected function get_sut(
-		?Settings_Interface $settings = null,
-		?LoggerInterface $logger = null,
-		?Bitcoin_Wallet_Repository $bitcoin_wallet_repository = null,
-		?Bitcoin_Address_Repository $bitcoin_address_repository = null,
+	protected LoggerInterface $logger;
+
+	protected function get_logger(): LoggerInterface {
+		if ( ! isset( $this->logger ) ) {
+			$this->logger = new ColorLogger();
+		}
+		return $this->logger;
+	}
+
+	protected function get_payment_service(
+		Blockchain_API_Extended_Interface $blockchain_api,
 		?Bitcoin_Transaction_Repository $bitcoin_transaction_repository = null,
-		?Blockchain_API_Interface $blockchain_api = null,
-		?Generate_Address_API_Interface $generate_address_api = null,
-		?Exchange_Rate_API_Interface $exchange_rate_api = null,
-		?Background_Jobs_Scheduler_Interface $background_jobs_scheduler = null,
-	): API {
-		$sut = new API(
-			settings: $settings ?? $this->makeEmpty( Settings_Interface::class ),
-			logger: $logger ?? new ColorLogger(),
-			bitcoin_wallet_repository: $bitcoin_wallet_repository ?? $this->make( Bitcoin_Wallet_Repository::class ),
-			bitcoin_address_repository: $bitcoin_address_repository ?? $this->make( Bitcoin_Address_Repository::class ),
-			bitcoin_transaction_repository: $bitcoin_transaction_repository ?? $this->make( Bitcoin_Transaction_Repository::class ),
-			blockchain_api: $blockchain_api ?? $this->makeEmpty( Blockchain_API_Interface::class ),
-			wallet_service: $generate_address_api ?? $this->makeEmpty( Generate_Address_API_Interface::class ),
-			exchange_rate_service: $exchange_rate_api ?? $this->makeEmpty( Exchange_Rate_API_Interface::class ),
-			background_jobs_scheduler: $background_jobs_scheduler ?? $this->makeEmpty( Background_Jobs_Scheduler_Interface::class ),
+	): Payment_Service {
+		return new Payment_Service(
+			blockchain_api: $blockchain_api,
+			bitcoin_transaction_repository: $bitcoin_transaction_repository
+				?? ( fn() => new Bitcoin_Transaction_Repository( new Bitcoin_Transaction_Factory() ) )(),
+			logger: $this->get_logger(),
 		);
-		return $sut;
 	}
 
 	/**
-	 * @return array<Blockchain_API_Interface[]>
+	 * @return array<Blockchain_API_Extended_Interface[]>
 	 */
 	public function dataprovider_get_bitcoin_apis(): array {
 		$result = array();
 
-		$logger = new ColorLogger();
-
-		$result[] = array( new Blockchain_Info_Api( $logger ) );
-		$result[] = array( new Blockstream_Info_API( $logger ) );
+		$result[] = array( new Blockchain_Info_Api_Extended( $this->get_logger() ) );
+		$result[] = array( new Blockstream_Info_API_Extended( $this->get_logger() ) );
 
 		return $result;
 	}
@@ -79,6 +68,8 @@ class Bitcoin_API_Contract_Test extends \lucatume\WPBrowser\TestCase\WPTestCase 
 	 * @dataProvider dataprovider_get_bitcoin_apis
 	 *
 	 * @param Blockchain_API_Interface $sut An instance of the API we are testing.
+	 * @throws JsonException
+	 * @throws Rate_Limit_Exception
 	 */
 	public function test_get_transactions_received( Blockchain_API_Interface $sut ): void {
 		$logger = new ColorLogger();
@@ -99,11 +90,15 @@ class Bitcoin_API_Contract_Test extends \lucatume\WPBrowser\TestCase\WPTestCase 
 	}
 
 	/**
+	 * Ensure our internal calculations match the
+	 *
 	 * @dataProvider dataprovider_get_bitcoin_apis
 	 *
-	 * @param Blockchain_API_Interface $blockchain_api An instance of the API we are testing.
+	 * @param Blockchain_API_Extended_Interface $blockchain_api An instance of the API we are testing.
+	 * @throws MoneyMismatchException
+	 * @throws Exception
 	 */
-	public function test_save_transactions_received( ?Blockchain_API_Interface $blockchain_api ): void {
+	public function test_save_transactions_received( Blockchain_API_Extended_Interface $blockchain_api ): void {
 
 		$raw_address = '3KKUGZk4yU9QfZZA9y9K5MkwBX7Rozaaum';
 
@@ -112,23 +107,16 @@ class Bitcoin_API_Contract_Test extends \lucatume\WPBrowser\TestCase\WPTestCase 
 			array(
 				'raw_address' => $raw_address,
 				'get_post_id' => 123,
+				'get_tx_ids'  => array(),
 			)
 		);
 
-		$bitcoin_address_factory     = new Bitcoin_Address_Factory();
-		$bitcoin_transaction_factory = new Bitcoin_Transaction_Factory();
-		$sut                         = $this->get_sut(
-			bitcoin_address_repository: new Bitcoin_Address_Repository( $bitcoin_address_factory ),
-			bitcoin_transaction_repository: new Bitcoin_Transaction_Repository( $bitcoin_transaction_factory ),
-			blockchain_api: $blockchain_api,
-		);
-		/** @var Transaction_Interface[] $result */
-		$result = $sut->update_address_transactions( $address );
+		$payment_service = $this->get_payment_service( $blockchain_api );
 
-		/** @var Transaction_Interface $first */
-		$first = array_pop( $result );
-		$ff    = $first->get_v_out();
+		$received = $blockchain_api->get_received_by_address( $raw_address, true );
 
-		$this->fail( 'incomplete' );
+		$result = $payment_service->check_address_for_payment( $address );
+
+		$this->assertTrue( $result->total_received->isEqualTo( $received ) );
 	}
 }
