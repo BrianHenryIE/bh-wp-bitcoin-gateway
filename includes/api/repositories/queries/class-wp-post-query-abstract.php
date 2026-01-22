48154112721be4b2fad67fcf5b0eb0becc9919f0
Brian Henry <BrianHenryIE@gmail.com>
2026-01-22 10:06:39 -0800

    Add `meta_input` to array shape

--- includes/api/repositories/queries/class-wp-post-query-abstract.php
+++ includes/api/repositories/queries/class-wp-post-query-abstract.php
@@ -17,7 +17,7 @@ use BackedEnum;
 use InvalidArgumentException;
 
 /**
- * @phpstan-type WpUpdatePostArray array{ID?: int, post_type?:string, post_status?:string, post_author?: int, post_date?: string, post_date_gmt?: string, post_content?: string, post_content_filtered?: string, post_title?: string, post_excerpt?: string}
+ * @phpstan-type WpUpdatePostArray array{ID?: int, post_type?:string, post_status?:string, post_author?: int, post_date?: string, post_date_gmt?: string, post_content?: string, post_content_filtered?: string, post_title?: string, post_excerpt?: string, meta_input?:array<string,mixed>}
  */
 abstract readonly class WP_Post_Query_Abstract {
 
