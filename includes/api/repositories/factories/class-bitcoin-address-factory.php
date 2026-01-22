f5ec231544459fd7371a4eed241860ac04a7d394
Brian Henry <BrianHenryIE@gmail.com>
2026-01-22 00:02:23 -0800

    Split into understandable functions

--- includes/api/repositories/factories/class-bitcoin-address-factory.php
+++ includes/api/repositories/factories/class-bitcoin-address-factory.php
@@ -46,45 +46,71 @@ class Bitcoin_Address_Factory {
 			post_id: $post->ID,
 			wallet_parent_post_id: $post->post_parent,
 			raw_address: $post->post_title,
-			derivation_path_sequence_number: ( function () use ( $post ) {
-				/** @var array|bool|float|int|resource|string|null|mixed $meta_value */
-				$meta_value = get_post_meta( $post->ID, Bitcoin_Address_WP_Post_Interface::DERIVATION_PATH_SEQUENCE_NUMBER_META_KEY, true );
-				return is_numeric( $meta_value ) ? intval( $meta_value ) : null;
-			} )(),
+			derivation_path_sequence_number: $this->get_derivation_path_sequence_number_from_post( $post ),
 			status: Bitcoin_Address_Status::from( $post->post_status ),
-			target_amount: ( function () use ( $post ): ?Money {
-				/** @var MoneySerializedArray|array{} $target_amount_meta */
-				$target_amount_meta = array_filter( (array) get_post_meta( $post->ID, Bitcoin_Address_WP_Post_Interface::TARGET_AMOUNT_META_KEY, true ) );
-				return empty( $target_amount_meta ) ? null : Money::of( ...$target_amount_meta );
-			} )(),
-			order_id: ( function () use ( $post ): ?int {
-				/** @var array|bool|float|int|resource|string|null|mixed $order_id_meta */
-				$order_id_meta = get_post_meta( $post->ID, Bitcoin_Address_WP_Post_Interface::ORDER_ID_META_KEY, true );
-				return is_numeric( $order_id_meta ) ? intval( $order_id_meta ) : null;
-			} )(),
-			tx_ids: ( function () use ( $post ): ?array {
-				/** @var array<int,string>|null|mixed $tx_ids_meta */
-				$tx_ids_meta = get_post_meta( $post->ID, Bitcoin_Address_WP_Post_Interface::TRANSACTIONS_META_KEY, true );
-				if ( is_array( $tx_ids_meta ) ) {
-					foreach ( $tx_ids_meta as $post_id => $tx_id ) {
-						if ( ! is_int( $post_id ) || ! is_string( $tx_id ) ) {
-							// TODO: Log error? Add a fail-hard debug flag?
-							return null;
-						}
-					}
-					/** @var array<int,string> $tx_ids_meta */
-					return $tx_ids_meta;
-				}
-				return null;
-			} )(),
+			target_amount: $this->get_target_amount_from_post( $post ),
+			order_id: $this->get_order_id_from_post( $post ),
+			tx_ids: $this->get_tx_ids_from_post( $post ),
 			// TODO: Use "received", not balance.
-			balance: ( function () use ( $post ): ?Money {
-				/** @var MoneySerializedArray|array{} $balance_meta */
-				$balance_meta = array_filter( (array) get_post_meta( $post->ID, Bitcoin_Address_WP_Post_Interface::BALANCE_META_KEY, true ) );
-				return empty( $balance_meta ) ? null : Money::of( ...$balance_meta );
-			} )(),
+			balance: $this->get_balance_from_post( $post ),
 		);
 
 		return $bitcoin_address;
+	}
+
+	/**
+	 * @param WP_Post $post The backing WP_Post for this Bitcoin_Address.
+	 */
+	protected function get_derivation_path_sequence_number_from_post( WP_Post $post ): ?int {
+		/** @var array|bool|float|int|resource|string|null|mixed $meta_value */
+		$meta_value = get_post_meta( $post->ID, Bitcoin_Address_WP_Post_Interface::DERIVATION_PATH_SEQUENCE_NUMBER_META_KEY, true );
+		return is_numeric( $meta_value ) ? intval( $meta_value ) : null;
+	}
+
+	/**
+	 * @param WP_Post $post The backing WP_Post for this Bitcoin_Address.
+	 */
+	protected function get_target_amount_from_post( WP_Post $post ): ?Money {
+		/** @var MoneySerializedArray|array{} $target_amount_meta */
+		$target_amount_meta = array_filter( (array) get_post_meta( $post->ID, Bitcoin_Address_WP_Post_Interface::TARGET_AMOUNT_META_KEY, true ) );
+		return empty( $target_amount_meta ) ? null : Money::of( ...$target_amount_meta );
+	}
+
+	/**
+	 * @param WP_Post $post The backing WP_Post for this Bitcoin_Address.
+	 */
+	protected function get_order_id_from_post( WP_Post $post ): ?int {
+		/** @var array|bool|float|int|resource|string|null|mixed $order_id_meta */
+		$order_id_meta = get_post_meta( $post->ID, Bitcoin_Address_WP_Post_Interface::ORDER_ID_META_KEY, true );
+		return is_numeric( $order_id_meta ) ? intval( $order_id_meta ) : null;
+	}
+
+	/**
+	 * @param WP_Post $post The backing WP_Post for this Bitcoin_Address.
+	 * @return array<int,string>|null
+	 */
+	protected function get_tx_ids_from_post( WP_Post $post ): ?array {
+		/** @var array<int,string>|null|mixed $tx_ids_meta */
+		$tx_ids_meta = get_post_meta( $post->ID, Bitcoin_Address_WP_Post_Interface::TRANSACTIONS_META_KEY, true );
+		if ( is_array( $tx_ids_meta ) ) {
+			foreach ( $tx_ids_meta as $post_id => $tx_id ) {
+				if ( ! is_int( $post_id ) || ! is_string( $tx_id ) ) {
+					// TODO: Log error? Add a fail-hard debug flag?
+					return null;
+				}
+			}
+			/** @var array<int,string> $tx_ids_meta */
+			return $tx_ids_meta;
+		}
+		return null;
+	}
+
+	/**
+	 * @param WP_Post $post The backing WP_Post for this Bitcoin_Address.
+	 */
+	protected function get_balance_from_post( WP_Post $post ): ?Money {
+		/** @var MoneySerializedArray|array{} $balance_meta */
+		$balance_meta = array_filter( (array) get_post_meta( $post->ID, Bitcoin_Address_WP_Post_Interface::BALANCE_META_KEY, true ) );
+		return empty( $balance_meta ) ? null : Money::of( ...$balance_meta );
 	}
 }
