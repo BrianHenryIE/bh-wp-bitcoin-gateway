a6e543ffe8d5c22fdae9be6ae6bfeb60050f8a64
Brian Henry <BrianHenryIE@gmail.com>
2026-01-27 21:08:00 -0800

    Add method to listen to `bh_wp_bitcoin_gateway_new_transactions_seen`

--- includes/integrations/woocommerce/class-order.php
+++ includes/integrations/woocommerce/class-order.php
@@ -2,15 +2,27 @@
 /**
  * Constants for order meta-keys.
  *
+ * Handles new transactions and payment actions.
+ *
  * @package    brianhenryie/bh-wp-bitcoin-gateway
  */
 
 namespace BrianHenryIE\WP_Bitcoin_Gateway\Integrations\WooCommerce;
 
+use BrianHenryIE\WP_Bitcoin_Gateway\API\Model\Wallet\Bitcoin_Address;
+use BrianHenryIE\WP_Bitcoin_Gateway\API\Services\Results\Check_Address_For_Payment_Service_Result;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
+use WC_Order;
+
 /**
  * Defines constants for meta-keys.
+ *
+ * @see WooCommerce_Integration::define_order_hooks()
  */
-interface Order {
+class Order implements LoggerAwareInterface {
+	use LoggerAwareTrait;
 
 	const string BITCOIN_ADDRESS_META_KEY = 'bh_wp_bitcoin_gateway_payment_address';
 
@@ -20,5 +32,50 @@ interface Order {
 
 	const string BITCOIN_AMOUNT_RECEIVED_META_KEY = 'bh_wp_bitcoin_gateway_bitcoin_amount_received';
 
-	const string LAST_CHECKED_META_KEY = 'bh_wp_bitcoin_gateway_last_checked_time';
+	/**
+	 * Constructor.
+	 *
+	 * @param API_WooCommerce_Interface $api_woocommerce Functions for querying/fetching crypto payment details for WooCommerce orders.
+	 * @param LoggerInterface           $logger PSR logger.
+	 */
+	public function __construct(
+		protected API_WooCommerce_Interface $api_woocommerce,
+		LoggerInterface $logger
+	) {
+		$this->setLogger( $logger );
+	}
+
+	/**
+	 * Record new transactions as order notes.
+	 *
+	 * @hooked bh_wp_bitcoin_gateway_new_transactions_seen
+	 *
+	 * @param string|class-string|null                 $integration_id Identifier for the integration the payment address was used by.
+	 * @param ?int                                     $order_post_id Identifier for the order the payment address was assigned to.
+	 * @param Bitcoin_Address                          $payment_address The address the transactions were found for.
+	 * @param Check_Address_For_Payment_Service_Result $check_address_for_payment_service_result The detail of existing and new transactions.
+	 */
+	public function new_transactions_seen(
+		?string $integration_id,
+		?int $order_post_id,
+		Bitcoin_Address $payment_address,
+		Check_Address_For_Payment_Service_Result $check_address_for_payment_service_result,
+	): void {
+
+		if ( WooCommerce_Integration::class !== $integration_id ) {
+			return;
+		}
+
+		$order = wc_get_order( $order_post_id );
+
+		if ( ! ( $order instanceof WC_Order ) ) {
+			// TODO: log... trashed?
+			return;
+		}
+
+		$this->api_woocommerce->add_order_note_for_transactions(
+			$order,
+			$check_address_for_payment_service_result->get_new_transactions()
+		);
+	}
 }
