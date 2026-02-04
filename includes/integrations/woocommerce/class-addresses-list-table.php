05d80c38ee1be2ef9ec22c9cdcaefb3e9a42ee5d
Brian Henry <BrianHenryIE@gmail.com>
2026-02-04 09:28:12 -0800

    Use `id` not `title` in url

--- includes/integrations/woocommerce/class-addresses-list-table.php
+++ includes/integrations/woocommerce/class-addresses-list-table.php
@@ -55,7 +55,7 @@ class Addresses_List_Table {
 		$gateway_instance = WC_Payment_Gateways::instance()->get_available_payment_gateways()[ $gateway_id ];
 
 		return array(
-			'href' => admin_url( sprintf( 'admin.php?page=wc-settings&tab=checkout&section=%s', $gateway_instance->title ) ),
+			'href' => admin_url( sprintf( 'admin.php?page=wc-settings&tab=checkout&section=%s', $gateway_instance->id ) ),
 			'text' => sprintf( 'WooCommerce: %s', $gateway_instance->title ),
 		);
 	}
